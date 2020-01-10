567fb5099a39b9ccd71495ef069c7e440a67e84f
Zac Cohan <zac@acqualia.com>
2020-01-10 11:45:11 +1100

    Initial Commit

---
+++ WindowControllerAutosaveTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WindowControllerAutosaveTest
+//
+//  Created by Zac Cohan on 10/1/20.
+//  Copyright © 2020 Zac Cohan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WindowControllerAutosaveTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindowControllerAutosaveTest
+//
+//  Created by Zac Cohan on 10/1/20.
+//  Copyright © 2020 Zac Cohan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
